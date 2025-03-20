5f5455a04b70046c671f11891955506ef625cc13
Caio Lacerda <lacerdacaiogabriel@gmail.com>
2025-03-20 11:02:33 -0300

    feat: add icons

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,31 +4,31 @@ import Input from "../../components/Input"
 import { useRouter } from "next/navigation"
 import styles from "./login.css"
 
-export default function login(){
+export default function login() {
     const router = useRouter();
-    
-return (
-    <div className="container">   
-    <div className="form">
-    <div className="text">
-        <h1>Login</h1>
-        <h6>Bem vindo ao Login!!</h6>
-    </div>
-    <div className="inputs">
-    <Input tipo="text" text="Username" />
-    <Input id="password" tipo="password" text="Password"/> 
-    <div className="button">
-        <button className="button" onClick={() => router.push("/profile")}>Login</button>
-    </div>
-    <nav className="button">
-        <button className="cadastro" onClick={() => router.push("/cadastro")}>Cadastro</button>
-    </nav>
-    </div>
-        <div className="b1"></div>
-        <div className="b2"></div>
-        <div className="b3"></div>
-        <div className="b4"></div>
-    </div>
-    </div>
-)
+
+    return (
+        <div className="container">
+            <div className="form">
+                <div className="text">
+                    <h1>Login</h1>
+                    <h6>Bem vindo ao Login!!</h6>
+                </div>
+                <div className="inputs">
+                    <Input tipo="text" text="Username" />
+                    <Input id="password" tipo="password" text="Password" />
+                    <div className="button">
+                        <button className="button" onClick={() => router.push("/profile")}>Login</button>
+                    </div>
+                    <nav className="button">
+                        <button className="cadastro" onClick={() => router.push("/cadastro")}>Cadastro</button>
+                    </nav>
+                </div>
+                <div className="b1"></div>
+                <div className="b2"></div>
+                <div className="b3"></div>
+                <div className="b4"></div>
+            </div>
+        </div>
+    )
 }
